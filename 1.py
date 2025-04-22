73f4071d547cb388f34fe14155cf7004665bbcc8
beinanengg <157193559+beinanengg@users.noreply.github.com>
2025-04-22 07:53:26 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -3,10 +3,10 @@ model = api.load("word2vec-google-news-300")
 
 queen_vector = model["king"] - model["man"] + model["woman"]
 print(queen_vector)
-
 similar_words = model.similar_by_vector(queen_vector, topn=1)
 print(similar_words)
 
 actor_vector = model["actor"] - model["man"] + model["woman"]
+print(actor_vector)
 similar_words = model.similar_by_vector(actor_vector, topn=5)
 print(similar_words)
